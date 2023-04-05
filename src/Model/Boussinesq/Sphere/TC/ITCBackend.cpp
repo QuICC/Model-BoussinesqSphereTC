715b0fc6d1aaaf8323cbdf34e382c29acab80994
Philippe Marti <philippe.marti@erdw.ethz.ch>
2023-04-05 13:58:53 +0200

    Move flags into abstract interface

--- src/Model/Boussinesq/Sphere/TC/ITCBackend.cpp
+++ src/Model/Boussinesq/Sphere/TC/ITCBackend.cpp
@@ -59,7 +59,7 @@ namespace Sphere {
 namespace TC {
 
    ITCBackend::ITCBackend()
-      : IModelBackend(), mUseGalerkin(false), mUseSplitEquation(false)
+      : IModelBackend()
    {
    }
 
@@ -88,26 +88,6 @@ namespace TC {
       std::vector<bool> periodic = {false, false, false};
 
       return periodic;
-   }
-
-   bool ITCBackend::useGalerkin() const
-   {
-      return this->mUseGalerkin;
-   }
-
-   void ITCBackend::enableGalerkin(const bool flag)
-   {
-      this->mUseGalerkin = flag;
-   }
-
-   bool ITCBackend::useSplitEquation() const
-   {
-      return this->mUseSplitEquation;
-   }
-
-   void ITCBackend::enableSplitEquation(const bool tag)
-   {
-      this->mUseSplitEquation = tag;
    }
 
    std::map<std::string,MHDFloat> ITCBackend::automaticParameters(const std::map<std::string,MHDFloat>& cfg) const

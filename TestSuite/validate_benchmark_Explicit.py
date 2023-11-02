50c0cecebe1b8fe785bbbb118769633ea043df66
Philippe Marti <philippe.marti@erdw.ethz.ch>
2023-11-02 13:40:42 +0100

    Tune tolerances for KK runs

--- TestSuite/validate_benchmark_Explicit.py
+++ TestSuite/validate_benchmark_Explicit.py
@@ -8,7 +8,7 @@ results = []
 
 # Tolerance per max rows
 rows = list(range(0, 101, 10))
-tols = [32, 65, 97, 130, 163, 179, 195, 260, 260, 228, 260]
+tols = [32, 65, 112, 130, 163, 179, 195, 260, 260, 238, 260]
 
 prefixes = ['temperature', 'kinetic']
 spectra = ['l', 'm', 'n']

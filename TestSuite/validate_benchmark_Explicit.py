3b7277a964eb767cb8013002d5a7bd6f369e4326
Philippe Marti <philippe.marti@erdw.ethz.ch>
2025-07-07 16:54:04 +0200

    Update ULP for LUMI-C (#32)

--- TestSuite/validate_benchmark_Explicit.py
+++ TestSuite/validate_benchmark_Explicit.py
@@ -8,7 +8,7 @@ results = []
 
 # Tolerance per max rows
 rows = list(range(0, 101, 10))
-tols = [32, 65, 112, 130, 163, 179, 195, 260, 260, 238, 260]
+tols = [32, 80, 112, 130, 163, 179, 195, 260, 260, 254, 260]
 
 prefixes = ['temperature', 'kinetic']
 spectra = ['l', 'm', 'n']
